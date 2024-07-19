23224362cfdf5b4736f6c66b849be35f3e426cb9
Jhoony <webwork.jhoony@gmail.com>
2024-07-19 11:58:04 +0700

    fix

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,20 +1,16 @@
-declare function calculateTax(
-    amount: number,
-    discount?: number,
-    ...extraFees: number[]
-): number
-declare const taxValue: number
+declare function calculateTax(amount: number, discount?: number, ...extraFees: number[]): number;
+declare const taxValue: number;
 interface Animal {
-    name: string
-    age: number
-    eat: [string, number?]
+    name: string;
+    age: number;
+    eat: [string, number?];
 }
-declare const test: Animal
+declare const test: Animal;
 declare class Cat {
-    name: string
-    constructor(name: string)
+    name: string;
+    constructor(name: string);
 }
 declare class Dog {
-    name: string
-    constructor(name: string)
+    name: string;
+    constructor(name: string);
 }
--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,27 @@
 function calculateTax(amount, discount = 0, ...extraFees) {
-    return (
-        amount * 1.2 -
+    return (amount * 1.2 -
         discount +
-        extraFees.reduce((total, val) => total + val, 0)
-    )
+        extraFees.reduce((total, val) => total + val, 0));
 }
-const taxValue = calculateTax(1, 0)
-console.log(`Tax value: ${taxValue}`)
+const taxValue = calculateTax(1, 0);
+console.log(`Tax value: ${taxValue}`);
 const test = {
     name: 'dsd',
     age: 2,
     eat: ['grass']
-}
+};
 class Cat {
-    name
+    name;
     constructor(name) {
-        this.name = name
+        this.name = name;
     }
 }
 class Dog {
-    name
+    name;
     constructor(name) {
-        this.name = name
+        this.name = name;
     }
 }
-new Cat('ni')
-new Dog('ni')
-console.log(test)
-//# sourceMappingURL=index.js.map
+new Cat('ni');
+new Dog('ni');
+console.log(test);
